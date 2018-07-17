694b438ab7f7f6ae3b6ccd6f7b89ef398421f09b
hesh <1035922783@qq.com>
2018-07-17 16:52:57 +0800

    删除

--- src/procedure/proc_student_test/tb_student_test_operate.sql
+++
@@ -1,7 +0,0 @@
-alter table tb_student_test
-      add constraint pk_student_test primary key(id);
-
-alter table tb_student_test add (address varchar2(50))
-alter table tb_student_test add (updatetime DATE)
-
-alter table tb_student_test add (lat number(9,6))
